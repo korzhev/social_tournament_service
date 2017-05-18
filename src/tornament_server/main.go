4ca1ecd1a165b76ed14303bfaec5f3b83aac7669
vk <amv12dbr9@gmail.com>
2017-05-18 22:44:55 +0300

    validation

--- src/tornament_server/main.go
+++ src/tornament_server/main.go
@@ -16,12 +16,16 @@ type OkResponse struct {
 
 const PlayerPointsErrMsg = "\"playerId\" and \"points\" are positive integers and required"
 
+func validationErrPidPoints(errPid error, errPoints error, pid uint64) bool {
+	return errPid != nil || errPoints != nil || pid == 0
+}
+
 func takeHandler(c echo.Context) error {
 	pidStr := c.QueryParam("playerId")
 	pid, errPid := strconv.ParseUint(pidStr, 10, 64)
 	pointsStr := c.QueryParam("points")
 	points, errPoints := strconv.ParseUint(pointsStr, 10, 64)
-	if errPid != nil || errPoints != nil {
+	if validationErrPidPoints(errPid, errPoints, pid) {
 		return &echo.HTTPError{http.StatusBadRequest, PlayerPointsErrMsg}
 	}
 	return c.JSON(http.StatusOK, &OkResponse{Message: fmt.Sprintf("%d points were taken from playerid: %d", points, pid)})
@@ -31,7 +35,7 @@ func fundHandler(c echo.Context) error {
 	pid, errPid := strconv.ParseUint(pidStr, 10, 64)
 	pointsStr := c.QueryParam("points")
 	points, errPoints := strconv.ParseUint(pointsStr, 10, 64)
-	if errPid != nil || errPoints != nil {
+	if validationErrPidPoints(errPid, errPoints, pid) {
 		return &echo.HTTPError{http.StatusBadRequest, PlayerPointsErrMsg}
 	}
 	return c.JSON(http.StatusOK, &OkResponse{Message: fmt.Sprintf("Funded %d points to playerid: %d", points, pid)})
@@ -42,6 +46,10 @@ func main() {
 	// routes
 	e.GET("/take", takeHandler)
 	e.GET("/fund", fundHandler)
+	e.GET("/announceTournament", fundHandler)
+	e.GET("/joinTournament", fundHandler)
+	e.GET("/balance", fundHandler)
+	e.POST("/resultTournament", fundHandler)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
